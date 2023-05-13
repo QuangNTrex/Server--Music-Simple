fc406c231a175b8c0f7d96ef0ff6d9cf1ae6e0e5
Tien Quang <quangnt.rex@gmail.com>
2023-05-13 13:33:09 +0700

    add header

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ app.use(
     credentials: true,
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 
 // app.set("trust proxy", 1);
